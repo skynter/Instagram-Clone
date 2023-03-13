4ca537c420076aff3cae4f88b46eed245d98b905
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2023-03-13 15:13:39 +0100

    Setup Completed

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase Initialization
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-C_jwbSxoL1hspuHvGNpMEwEGE4ukY1U",
+  authDomain: "instagram-clone-c9891.firebaseapp.com",
+  projectId: "instagram-clone-c9891",
+  storageBucket: "instagram-clone-c9891.appspot.com",
+  messagingSenderId: "470836582010",
+  appId: "1:470836582010:web:15d1cad66a3d8b896b4dc3",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
